dd313ec0fc833f29c41a83c87c15e99c006e176c
Cyle <ccbenda2696@gmail.com>
2025-07-20 21:31:05 -0600

    New CTA file

---
+++ PersonalInfo.java
@@ -0,0 +1,17 @@
+public class PersonalInfo {
+
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "246 Elm Street";
+        String city = "LalaLand";
+        String zipCode = "24680";
+
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip code: " + zipCode);
+    
+    }
+}
